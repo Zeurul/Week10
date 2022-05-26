3eaeda1a91ab87588885222f8726aa42d93f4cf4
Zeurul <105402084+Zeurul@users.noreply.github.com>
2022-05-25 20:20:01 -0500

    Update week10.py

--- week10.py
+++ week10.py
@@ -1,3 +1,8 @@
 import OS
-import json
 
+directory = input('Enter the directory name:')
+filename = directory+ input ('Enter the filename:')
+
+name = input('Enter name:')
+address = input('Enter Address:')
+phone = input('Enter phone number:')
