19f40d3ebfa6bf8e33354538e75cb82e5c5f654e
Zeurul <105402084+Zeurul@users.noreply.github.com>
2022-05-25 20:24:37 -0500

    Update week10.py

--- week10.py
+++ week10.py
@@ -6,3 +6,21 @@ filename = directory+ input ('Enter the filename:')
 name = input('Enter name:')
 address = input('Enter Address:')
 phone = input('Enter phone number:')
+
+#creates directory
+os.mkdir (directory)
+
+#creates the file within the directory
+with open("{}/{}.csv".format(directory, filename), 'w') as file:
+    file.write(",".join([name, address, phone]) + "n")
+
+# create file object to read from specified file in directory
+with open("{}/{}.csv".format(directory, filename), 'r') as file:
+
+  print("{}/{}.csv contents".format(directory, filename))
+
+  # loop through each line in file and print to screen
+
+  for line in file:
+
+    print(line)
